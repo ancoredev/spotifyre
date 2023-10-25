c50f64fca839e3907f5ec238c55dfb67e9108cfc
Khairov Marat <khairovm9975@gmail.com>
2023-10-25 11:21:20 +0300

    player fix

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -1,5 +1,6 @@
 'use client'
 import useLoadImage from '@/hooks/useLoadImage';
+import usePlayer from '@/hooks/usePlayer';
 import { Song } from '@/types';
 import Image from 'next/image';
 import { FC } from 'react'
@@ -15,13 +16,15 @@ const MediaItem: FC<MediaItemProps> = ({
   onClick
 }) => {
   const imageUrl = useLoadImage(data);
+  const player = usePlayer();
 
   const handleClick = () => {
     if (onClick) {
       return onClick(data.id)
     }
 
-    // TODO: Defaut turn on player
+    // default turn on player
+    return player.setId(data.id)
   }
   return (
     <div
